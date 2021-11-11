dfe8413f834151a3fe836fde5069889609cde18c
sandor.juhasz <sandor.juhasz.1983@gmail.com>
2021-11-11 07:12:40 +0100

    Silent oh-my-zsh install.

--- NON-STOW/zsh/install.sh
+++ NON-STOW/zsh/install.sh
@@ -7,11 +7,19 @@ function get_current_shell() {
     grep "^${USER}" /etc/passwd | cut -d : -f 7
 }
 
+echo "Changing the shell.."
 if [[ "$(get_current_shell)" != "/usr/bin/zsh" ]]; then
-    echo "Current shell is not zsh. Installing oh-my-zsh."
-    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+    chsh /usr/bin/zsh
+else
+    echo "Current shell is ZSH, doing nothing."
+fi
+
+echo "Checking oh-my-zsh installation..."
+if [[ ! -e ~/.oh-my-zsh ]]; then
+    echo "Installing oh-my-zsh."
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
     rm ~/.zshrc
 else
-    echo "Zsh found, doing nothing."
+    echo "Oh-my-zsh found, doing nothing."
 fi
 
