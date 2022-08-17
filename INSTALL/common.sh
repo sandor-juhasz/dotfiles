b86b7ddfa25730bf0e30d2d7485b7d38e7556775
sandor-juhasz <sandor.juhasz.1983@gmail.com>
2022-08-17 17:41:16 +0200

    Refactoring installers, creating common functions for vscode extension management.

--- INSTALL/common.sh
+++ INSTALL/common.sh
@@ -9,3 +9,25 @@ function main() {
         echo "Usage: $(basename $0) {install|uninstall}"
     fi    
 }
+
+function install_vscode_extension() {
+    local _plugin=$1
+
+    if which code; then
+        echo "Installing VSCode plugin..."
+       code --install-extension "${_plugin}" --force
+    else
+       echo "Visual Studio code was not found, skipping plugin installation."
+    fi    
+}
+
+function uninstall_vscode_extension() {
+    local _plugin=$1
+
+    if which code; then
+       echo "Removing VSCode plugin"
+       code --uninstall-extension "${_plugin}" --force
+    else
+       echo "Visual Studio code was not found, skipping plugin uninstall."
+    fi
+}
