d5ec65213e6a9cf23d126567b6f9e4541ed1fb5c
sandor-juhasz <sandor.juhasz.1983@gmail.com>
2022-08-17 17:28:48 +0200

    Refactoring installers, moving main method to common.sh

---
+++ INSTALL/common.sh
@@ -0,0 +1,9 @@
+function main() {
+    if [[ $1 == "install" ]]; then
+        install
+    elif [[ $1 == "uninstall" ]]; then
+        uninstall
+    else
+        echo "Usage: $(basename $0) {install|uninstall}"
+    fi    
+}
