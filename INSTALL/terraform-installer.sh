bee1de92ecc54721dbc004c4d2d801620f0e7470
sandor-juhasz <sandor.juhasz.1983@gmail.com>
2022-08-18 08:55:52 +0200

    Installer for Terraform

---
+++ INSTALL/terraform-installer.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+source ./common.sh
+
+
+VSCODE_EXTENSION="hashicorp.terraform"
+
+function install() {
+    # Installation steps are added from https://www.terraform.io/downloads
+
+    # Installing prerequisites
+    sudo apt-get update   
+    sudo apt-get -y install ca-certificates curl gnupg lsb-release
+
+    # Installing Docker's official GPG key
+    sudo mkdir -p /etc/apt/keyrings
+    curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/hashicorp-archive-keyring.gpg
+
+    # Setting up the repository
+    echo "deb [signed-by=/etc/apt/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
+
+    # Installing Terraform
+    sudo apt-get update 
+    sudo apt-get -y install terraform
+
+    install_vscode_extension "${VSCODE_EXTENSION}"
+}
+
+function uninstall() {
+    echo "Uninstalling Terraform..."
+    sudo apt-get -y remove terraform
+    sudo rm /etc/apt/sources.list.d/hashicorp.list
+    sudo rm /etc/apt/keyrings/hashicorp-archive-keyring.gpg
+    sudo apt-get update
+
+    uninstall_vscode_extension "${VSCODE_EXTENSION}"
+}
+
+main $1
