ea3d421ee580654afd5dfc819153b9c90bbf92d4
sandor-juhasz <sandor.juhasz.1983@gmail.com>
2023-08-30 11:43:13 +0200

    Disabling oh-my-zsh prompt, updating zsh installer.

--- NON-STOW/zsh/install.sh
+++ NON-STOW/zsh/install.sh
@@ -3,23 +3,22 @@
 ##########################################################
 # Returns the default shell for the current user.
 ##########################################################
-# function get_current_shell() {
-#     grep "^${USER}:" /etc/passwd | cut -d : -f 7
-# }
+function get_current_shell() {
+    grep "^${USER}:" /etc/passwd | cut -d : -f 7
+}
 
-# echo "Changing the shell.."
-# if [[ "$(get_current_shell)" != "/usr/bin/zsh" ]]; then
-#     echo "Changing the default shell to ZSH. Please enter your password when prompted."
-#     chsh "${USER}" -s /usr/bin/zsh
-# else
-#     echo "Current shell is ZSH, doing nothing."
-# fi
+echo "Current shell is $SHELL. Changing the default shell to Zsh.."
+if [[ "$(get_current_shell)" != "/usr/bin/zsh" ]]; then
+    sudo chsh --shell /bin/zsh vscode 
+else
+    echo "Current shell is ZSH, doing nothing."
+fi
 
-# echo "Checking oh-my-zsh installation..."
-# if [[ ! -e ~/.oh-my-zsh ]]; then
-#     echo "Installing oh-my-zsh."
-#     sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
-#     rm ~/.zshrc
-# else
-#     echo "Oh-my-zsh found, doing nothing."
-# fi
+echo "Checking oh-my-zsh installation..."
+if [[ ! -e ~/.oh-my-zsh ]]; then
+    echo "Installing oh-my-zsh."
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+    rm ~/.zshrc
+else
+    echo "Oh-my-zsh found, doing nothing."
+fi
---
+++ zsh/.config/zshrc.d/oh-my-zsh.sh
@@ -0,0 +1,4 @@
+# Customized Oh-my-zsh settings
+
+DISABLE_AUTO_UPDATE=true
+DISABLE_UPDATE_PROMPT=true
