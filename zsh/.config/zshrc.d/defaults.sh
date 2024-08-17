97deaf20074aa8e1ebec61a4597846b174a874f4
sandor-juhasz <sandor.juhasz.1983@gmail.com>
2024-08-17 12:28:32 +0200

    Fixing BROWSER in devcontainers

--- zsh/.config/zshrc.d/defaults.sh
+++ zsh/.config/zshrc.d/defaults.sh
@@ -6,8 +6,10 @@ function is_wsl() {
     grep -qEi "(Microsoft|WSL)" /proc/version &> /dev/null
 }
 
-if is_wsl; then
-    export BROWSER='/mnt/c/Program Files/Google/Chrome/Application/chrome.exe'
+if [[ is_wsl && ! -z "${REMOTE_CONTAINERS}" ]]; then
+    if [[ -e '/mnt/c/Program Files/Google/Chrome/Application/chrome.exe' ]]; then 
+        export BROWSER='/mnt/c/Program Files/Google/Chrome/Application/chrome.exe'
+    fi
 fi
 
 export EDITOR=code
